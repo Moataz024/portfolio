f520acbc74d325c9ed879c8d9fefed94ec808787
Moataz024 <prexzcod@gmail.com>
2025-01-05 20:08:02 +0100

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Experience from './components/Experience';
 import Education from './components/Education';
 import Contact from './components/Contact';
 import Background from './components/Background';
+import GitHubStats from './components/GitHubStats';
 
 function App() {
   return (
@@ -20,6 +21,12 @@ function App() {
       <Projects />
       <Experience />
       <Education />
+      <div className="py-24 px-4" id="github-stats">
+        <div className="max-w-4xl mx-auto">
+          <h2 className="text-3xl font-light text-white mb-16 text-center">GitHub Statistics</h2>
+          <GitHubStats />
+        </div>
+      </div>
       <Contact />
     </div>
   );
